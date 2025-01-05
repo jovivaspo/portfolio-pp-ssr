52754b665c4aab07ae0872a788c282a879b663c4
jovivaspo <jorgevipo.dev@gmail.com>
2025-01-05 21:53:35 +0100

    Change margin gallery video

--- app/videography/page.tsx
+++ app/videography/page.tsx
@@ -6,7 +6,7 @@ export default function VideographyPage() {
     <div className='w-full h-full flex flex-col md:gap-24 gap-16 mb-24'>
       <HeaderHero type='video' src='https://res.cloudinary.com/dme5pqzrj/video/upload/v1698424387/portafolio-pp/videography-cover_relqfp.mp4' title='Videography' />
 
-      <div className='w-full flex flex-col justify-center items-center mt-24 px-8'>
+      <div className='w-full flex flex-col justify-center items-center mt-24 md:px-8'>
         <div className='my-2 flex flex-col gap-2'>
           <h2 className='text-center font-bold'>Videography Gallery</h2>
           <h3 className='text-center text-my-gray'>Visual creations ranging from dynamic animations to professional recordings.</h3>
